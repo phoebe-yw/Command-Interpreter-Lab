43a6ae3d8166ae619b73b8a8de6777cc95480369
phoebe-yw <yifeiwang0613@gmail.com>
2026-01-18 21:59:17 -0600

    passed nop testcases

--- src/ci/interpreter.c
+++ src/ci/interpreter.c
@@ -2,49 +2,77 @@
  */
 
 #include "interpreter.h"
+
 #include <inttypes.h>
 #include <stdio.h>
 
-void interpreter_init(Interpreter *intr) {
+void interpreter_init(Interpreter* intr) {
+    if (!intr) {
+        return;
+    }
     // Initialize interpreter state
     // Set all variables to 0, reset flags, etc.
+    for (int i = 0; i < NUM_VARIABLES; i++) {
+        intr->variables[i] = 0;
+    }
+    intr->had_error = false;
+    intr->is_greater = false;
+    intr->is_less = false;
+    intr->is_equal = false;
 }
 
-void interpret(Interpreter *intr, Command *commands) {
-  if (!intr || !commands) {
-    return;
-  }
+void interpret(Interpreter* intr, Command* commands) {
+    if (!intr || !commands) {
+        return;
+    }
 
-  // TODO: Implement the interpreter logic here.
-  // Loop through commands, execute them and update interpreter state.
-  // Should look a lot like the logic in `parser.c`, except now with command types as opposed to token types.
+    // TODO: Implement the interpreter logic here.
+    // Loop through commands, execute them and update interpreter state.
+    // Should look a lot like the logic in `parser.c`, except now with command
+    // types as opposed to token types.
+    Command* current = commands;
+    while (current != NULL) {
+        switch (current->type) {
+            case CMD_NOP:
+                break;
+            case CMD_ADD:
+            case CMD_SUB:
+            case CMD_MOV:
+            case CMD_CMP:
+            case CMD_CMP_U:
+            case CMD_PRINT:
+            default:
+                return;
+        }
+        current = current->next;
+    }
 }
 
-void print_interpreter_state(Interpreter *intr) {
-  if (!intr) {
-    return;
-  }
-
-  printf("Error: %d\n", intr->had_error);
-  printf("Flags:\n");
-  printf("Is greater: %d\n", intr->is_greater);
-  printf("Is equal: %d\n", intr->is_equal);
-  printf("Is less: %d\n", intr->is_less);
-
-  printf("\n");
-
-  printf("Variable values:\n");
-  for (size_t i = 0; i < NUM_VARIABLES; i++) {
-    printf("x%zu: %" PRId64 "", i, intr->variables[i]);
-
-    if (i < NUM_VARIABLES - 1) {
-      printf(", ");
+void print_interpreter_state(Interpreter* intr) {
+    if (!intr) {
+        return;
     }
 
-    if ((i + 1) % 8 == 0) {
-      printf("\n");
-    }
-  }
+    printf("Error: %d\n", intr->had_error);
+    printf("Flags:\n");
+    printf("Is greater: %d\n", intr->is_greater);
+    printf("Is equal: %d\n", intr->is_equal);
+    printf("Is less: %d\n", intr->is_less);
 
-  printf("\n");
+    printf("\n");
+
+    printf("Variable values:\n");
+    for (size_t i = 0; i < NUM_VARIABLES; i++) {
+        printf("x%zu: %" PRId64 "", i, intr->variables[i]);
+
+        if (i < NUM_VARIABLES - 1) {
+            printf(", ");
+        }
+
+        if ((i + 1) % 8 == 0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
 }
--- src/ci/parser.c
+++ src/ci/parser.c
@@ -11,19 +11,21 @@
 static bool parse_number(Token token, int64_t* result);
 static Command* parse_cmd(Parser* parser);
 
-void parser_init(Parser* parser, Lexer* lexer) {
+void parser_init(Parser* parser,
+                 Lexer* lexer) {  // we have the parser reading from the lexer
     if (!parser) {
         return;
     }
 
-    parser->lexer = lexer;
+    parser->lexer = lexer;  // set the lexer
     parser->had_error = false;
-    parser->current = lexer_next_token(parser->lexer);
-    parser->next = lexer_next_token(parser->lexer);
+    parser->current = lexer_next_token(parser->lexer);  // first token
+    parser->next = lexer_next_token(parser->lexer);     // second token
 }
 
 /**
- * @brief Parses the given value as a number.
+ * @brief Parses the given value as a number. (checks if token is a number and
+ * converts to int64_t)
  *
  * @param token The token to parse.
  * @param result A pointer to the value to modify on success.
@@ -53,10 +55,15 @@ static bool parse_number(Token token, int64_t* result) {
  * returned command.
  */
 static Command* parse_cmd(Parser* parser) {
+    if (!parser) {
+        return NULL;
+    }
+
     Token token = parser->current;
 
     if (token.type == TOK_EOF) {
         // We are at the end and there are no commands left to parse.
+        return NULL;
     }
 
     // TODO: Switch over token types and parse appropriately.
@@ -65,9 +72,34 @@ static Command* parse_cmd(Parser* parser) {
     // See `include/ci/token_type.h` for token types.
     //
     // Token types correspond to commands, e.g., TOK_NOP -> CMD_NOP.
+    Command* cmd = malloc(sizeof(Command));
     switch (token.type) {
+        case TOK_NOP:
+            cmd->type = CMD_NOP;
+            cmd->next = NULL;
+
+            parser->current = parser->next;                  // becomes NL
+            parser->next = lexer_next_token(parser->lexer);  // becomes nop
+
+            // if (parser->current.type == TOK_NL) {
+            //     parser->current = parser->next;
+            //     parser->next = lexer_next_token(parser->lexer);
+            // } else if (parser->current.type != TOK_EOF) {
+            //     parser->had_error = true;  // free after
+            //     free(cmd);
+            //     return NULL;
+            // }
+            if (parser->current.type != TOK_NL &&
+                parser->current.type != TOK_EOF) {
+                parser->had_error = true;  // free after
+                free(cmd);
+                return NULL;
+            }
+            return cmd;
         default:
+            // unrecognized command
             parser->had_error = true;
+            free(cmd);
             break;
     }
 
@@ -78,5 +110,31 @@ Command* parse_commands(Parser* parser) {
     // TODO: Build a linked list of commands by repeatedly calling parse_cmd.
     //
     // For now, we try and parse a single command.
-    return parse_cmd(parser);
+    if (!parser) {
+        return NULL;
+    }
+    Command* head = NULL;
+    Command* tail = NULL;  // keep track of last command added
+    while (!parser->had_error) {
+        if (parser->current.type == TOK_EOF) {
+            break;
+        }
+        // skip newlines
+        while (parser->current.type == TOK_NL) {
+            parser->current = parser->next;
+            parser->next = lexer_next_token(parser->lexer);
+        }
+        Command* new_cmd = parse_cmd(parser);
+        if (!new_cmd) {
+            break;
+        }
+        if (head == NULL) {
+            head = new_cmd;
+            tail = new_cmd;
+        } else {
+            tail->next = new_cmd;
+            tail = new_cmd;
+        }
+    }
+    return head;
 }
